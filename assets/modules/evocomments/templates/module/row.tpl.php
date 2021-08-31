63d03d4f08629e63ca4775e5b810d784dff7727a
Bilbo Baggins <d@iliukhin.com>
2021-09-01 00:59:50 +0300

    Update row.tpl.php
    add premoderate

--- assets/modules/evocomments/templates/module/row.tpl.php
+++ assets/modules/evocomments/templates/module/row.tpl.php
@@ -5,6 +5,7 @@
   <td><span title="[+created_at+]">[+date+]</span></td>
   <td style="text-align:center;">  
     <button class="btn btn-info" data-evocomments-action="restoreComment"><i class="fa fa-refresh"></i></button>
+    <button class="btn btn-success" data-evocomments-action="publishComment"><i class="fa fa-check"></i></button>
     <button class="btn btn-danger" data-evocomments-action="destroyComment"><i class="fa fa-trash"></i></button>
     <button class="btn btn-danger" data-evocomments-action="deleteComment"><i class="fa fa-remove"></i></button>
   </td>
